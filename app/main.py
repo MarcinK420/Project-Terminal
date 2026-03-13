1ea853acd82874de98c627db6fdf4de37b19005b
Marcin Kruk <mkruk@CEFARM.local>
2026-03-13 14:22:49 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,17 +1,35 @@
 import sys
 import os
+import subprocess
+
+def check_path(variable):
+        system_path = os.environ.get('PATH', '')
+        for folder in system_path.split(os.pathsep):
+            full_path = os.path.join(folder, variable)
+            if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+                return full_path
+            else:
+                return None
 
 def type(command):
     built_in_commands = ['echo', 'exit', 'type']
     if command[5:] in built_in_commands:
         return f"{command[5:]} is a shell builtin" 
     else:
-        system_path = os.environ.get('PATH', '')
-        for folder in system_path.split(os.pathsep):
-            full_path = os.path.join(folder, command[5:])
-            if os.path.exists(full_path) and os.access(full_path, os.X_OK):
-                return f"{command[5:]} is {full_path}"
-    return f"{command[5:]}: not found"    
+        if check_path(command[5:]):
+            return f"{command[5:]} is {check_path(command[5:])}"
+    return f"{command[5:]}: not found" 
+
+def executable(command):
+    result = command.split()
+    filename = result[0]
+    arguments = result[1:]
+    if check_path(filename):
+        exe_path = check_path(filename)
+        output = subprocess.run([exe_path] + arguments, capture_output=True, text=True)
+        return output.stdout
+    return None
+
 
 def main():
     while True:
@@ -23,6 +41,8 @@ def main():
             print(command[5:])
         elif command[:4] == 'type':
             print(type(command))
+        elif executable(command):
+            print(executable(command))
         else:
             print(f"{command}: command not found")
 
