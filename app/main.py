bce7dc9e8a7a7b803bac72edc345e83b00e64045
Marcin Kruk <mkruk@CEFARM.local>
2026-03-13 11:51:33 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import sys
 
+def command_not_found(command):
+    return f"{command}: command not found"
+
+def type(command):
+    built_in_commands = ['echo', 'exit', 'type']
+    if command[5:] in built_in_commands:
+        return f"{command[5:]} is a shell builtin" 
+    else:
+        return command_not_found(command[5:])     
+
 def main():
     while True:
         sys.stdout.write("$ ")
@@ -8,8 +18,10 @@ def main():
             break
         elif command[:4] == 'echo':
             print(command[5:])
+        elif command[:4] == 'type':
+            print(type(command))
         else:
-            print(f"{command}: command not found")
+            print(command_not_found(command))
 
 
 if __name__ == "__main__":
