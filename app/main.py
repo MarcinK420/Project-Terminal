d5bc4d52f7c9329b241557077abfc3cb61a4b7b6
Marcin Kruk <mkruk@CEFARM.local>
2026-03-13 11:35:01 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 import sys
 
-
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command.lower() == 'exit':
+        if command == 'exit':
             break
-        print(f"{command}: command not found")
-        pass
+        elif command[:4] == 'echo':
+            print(command[5:])
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
