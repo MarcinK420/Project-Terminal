6936c8d459611b957914c7128d2b6dff1caf8c3d
Marcin Kruk <mkruk@CEFARM.local>
2026-03-26 15:49:47 +0100

    codecrafters submit [skip ci]

--- app/pipeline.py
+++ app/pipeline.py
@@ -11,23 +11,52 @@ def pipeline(command):
     
     for i, cmd_text in enumerate(commands_text):
         cmd = shlex.split(cmd_text) # Parsujemy np. 'head -n 3' -> ['head', '-n', '3']
-
         is_last = (i == len(commands_text) - 1)
-        
-        if prev_process:
-            current_stdin = prev_process.stdout
-        else:
-            current_stdin = None
-            
-        if is_last:
-            current_stdout = None
-        else:
-            current_stdout = subprocess.PIPE
 
-        current_process = subprocess.Popen(cmd, stdin=current_stdin, stdout=current_stdout)  # <- Tu musisz uzupełnić!
+        if cmd[0] in built_ins:
+            out_text = ''
+
+            if cmd[0] == 'echo':
+                out_text = quoting.echo(cmd_text) + '\n'
+            elif cmd[0] == 'type':
+                from app import main
+                out_text = main.type(cmd_text.strip()) + '\n'
+            
+            if prev_process:
+                prev_process.communicate()
+
+            if is_last:
+                print(out_text, end='')
+                current_process = None
+            else:
+                import os
+                r, w = os.pipe()
+                os.write(w, out_text.encode())
+                os.close(w)
+
+                class DummyProces:
+                    def __init__(self, fd):
+                        self.stdout = open(fd, 'rb')
+                    def communicate(self):
+                        pass
+
+                current_process = DummyProces(r)
         
-        if prev_process:
-            prev_process.stdout.close()
+        else:
+            if prev_process:
+                current_stdin = prev_process.stdout
+            else:
+                current_stdin = None
+                
+            if is_last:
+                current_stdout = None
+            else:
+                current_stdout = subprocess.PIPE
+
+            current_process = subprocess.Popen(cmd, stdin=current_stdin, stdout=current_stdout)  # <- Tu musisz uzupełnić!
+            
+            if prev_process:
+                prev_process.stdout.close()
 
         prev_process = current_process
 
