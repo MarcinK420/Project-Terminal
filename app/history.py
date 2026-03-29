298554b51d3fdf2d12f12623de92d003b8baf7eb
MarcinK420 <156346564+MarcinK420@users.noreply.github.com>
2026-03-29 17:35:26 +0200

    codecrafters submit [skip ci]

--- app/history.py
+++ app/history.py
@@ -6,12 +6,12 @@ def history(command,user_history):
         num_commands = int(commands[1])
         for index, command in enumerate(user_history[-num_commands:], start=len(user_history) - num_commands + 1):
             print(f"{index} {command}")
-    elif len(commands) > 1 and commands[1] == '-r':
-        path_to_file = commands[2]
+    elif len(commands) > 1 and user_history[-1] == '-r':
+        path_to_file = user_history[-1].split()[-1]
         try:
             with open(path_to_file, 'r') as file:
-                for line in file:
-                    print(line.strip())
+                for index, line in enumerate(file, start=1):
+                    print(f"{index} {line.strip()}")
         except FileNotFoundError:
             print(f"File not found: {path_to_file}")
     else:
