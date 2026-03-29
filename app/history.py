b644c62ac52e5fdbb3269bb09cfa10ac6a6cfbaf
MarcinK420 <156346564+MarcinK420@users.noreply.github.com>
2026-03-29 12:21:23 +0200

    codecrafters submit [skip ci]

--- app/history.py
+++ app/history.py
@@ -2,7 +2,7 @@ import shlex
 
 def history(command,user_history):
     commands = shlex.split(command)
-    if commands[1].isdigit():
+    if len(commands) > 1 and commands[1].isdigit():
         num_commands = int(commands[1])
         for index, command in enumerate(user_history[-num_commands:], start=len(user_history) - num_commands + 1):
             print(f"{index} {command}")
