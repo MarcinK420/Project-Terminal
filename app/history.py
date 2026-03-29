251ca011ae832d9c7b290074386c5caa82856a71
MarcinK420 <156346564+MarcinK420@users.noreply.github.com>
2026-03-29 17:45:14 +0200

    codecrafters submit [skip ci]

--- app/history.py
+++ app/history.py
@@ -7,15 +7,29 @@ def history(command,user_history):
         for index, command in enumerate(user_history[-num_commands:], start=len(user_history) - num_commands + 1):
             print(f"{index} {command}")
     elif len(commands) > 1 and commands[1] == '-r':
-        path_to_file = commands[2]
-        user_history.clear()
-        user_history.append(f"history -r {path_to_file}")
-        try:
-            with open(path_to_file, 'r') as file:
-                for line in file:
-                    user_history.append(line.strip())
-        except FileNotFoundError:
-            print(f"File '{path_to_file}' not found.")
+        history_read_file(commands,user_history)
+    elif len(commands) > 1 and commands[1] == '-w':
+        history_write_file(commands,user_history)
     else:
         for index, command in enumerate(user_history, start=1):
             print(f"{index} {command}")
+
+def history_read_file(commands,user_history):
+    path_to_file = commands[2]
+    user_history.clear()
+    user_history.append(f"history -r {path_to_file}")
+    try:
+        with open(path_to_file, 'r') as file:
+            for line in file:
+                user_history.append(line.strip())
+    except FileNotFoundError:
+        print(f"File '{path_to_file}' not found.")
+
+def history_write_file(commands,user_history):
+    path_to_file = commands[2]
+    try:
+        with open(path_to_file, 'w') as file:
+            for command in user_history:
+                file.write(command + '\n')
+    except Exception as e:
+        print(f"Error writing to file '{path_to_file}': {e}")
