d2c234edc2c75d1f0ab7972e1112e1ecec0bd3d9
MarcinK420 <156346564+MarcinK420@users.noreply.github.com>
2026-03-31 09:45:39 +0200

    Clean up your_program.sh header comments
    Removed comments and header from the script.

--- your_program.sh
+++ your_program.sh
@@ -1,11 +1,3 @@
-#!/bin/sh
-#
-# Use this script to run your program LOCALLY.
-#
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-#
-# Learn more: https://codecrafters.io/program-interface
-
 set -e # Exit early if any commands fail
 
 # Copied from .codecrafters/run.sh
